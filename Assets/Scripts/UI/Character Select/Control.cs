009758673cf370cf0f2bac47d557f3241c6b199d
Simon Armstrong <simon.armstrong@ad.aie.edu>
2016-10-21 16:57:24 +1100

    Character Select UI

---
+++ Assets/Scripts/UI/Character Select/Control.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Control : MonoBehaviour {
+
+    public Color filteredColor;
+    public List<PlayerPanel> playerSelectImages = new List<PlayerPanel>(); 
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 4; i++) {
+            if (!playerSelectImages[i].on) {
+                playerSelectImages[i].image.color = filteredColor;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ Assets/Scripts/UI/Character Select/PlayerPanel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerPanel : MonoBehaviour {
+    public bool on;
+    public Image image;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
