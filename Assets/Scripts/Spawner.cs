ec2e2ece130e47710d9d12bbcd167e8cc1b09098
Alex Armstrong <alexander.armst@ad.aie.edu>
2016-10-21 15:33:52 +1100

    Pushing the pointOrb spawning script

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour {
+    public List<GameObject> objectsToSpawn;
+    public int maxSpawnAmount = 10;
+    public int currentSpawned;
+
+    void Start()
+    {
+        Vector3 center = transform.position;
+        for (int i = 0; i < maxSpawnAmount; i++)
+        {
+            Vector3 pos = RandomCircle(center, i, 40.0f);
+            Instantiate(objectsToSpawn[objectsToSpawn.Count - 1], pos, Quaternion.identity);
+        }
+    }
+
+    Vector3 RandomCircle(Vector3 center, int na,  float radius)
+    {
+        float ang = UnityEngine.Random.Range(0,360);
+        radius = Random.Range(0, radius);
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + 0.6f;
+        pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        return pos;
+    }
+    
+
+    void FixedUpdate()
+    { 
+    }
+}
