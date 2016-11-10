7183e990d6f92da23f6559a3dcb6e8d8776919d3
Simon Armstrong <simon.armstrong@ad.aie.edu>
2016-11-10 14:48:37 +1100

    Player can change armour sets

---
+++ Assets/Scripts/SimpleLookAt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleLookAt : MonoBehaviour {
+    public GameObject target;
+
+    void Start() {
+        if (target == null) target = GameObject.FindWithTag("pointOrb");
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if(target != null) transform.LookAt(target.transform);
+	}
+}
